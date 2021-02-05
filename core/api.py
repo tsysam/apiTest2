39071db2a9c6f132dd3a873791210f1251309c85
amorzeev <aleksandr.morzeev@t-systems.com>
2021-02-05 14:32:02 +0300

    check separator

--- core/api.py
+++ core/api.py
@@ -5,12 +5,12 @@ from allure_commons._allure import step
 
 class Api:
     parser = configparser.ConfigParser()
-    # parser.read('../config.ini')
+    parser.read('../config.ini')
     # parser.read('config.ini')
-    parser.read('..\\config.ini')
+    # parser.read('..\\config.ini')
 
     BASE_URL = parser.get('backend', 'url')
-    GET_URL = BASE_URL + "/get"
+    GET_URL = BASE_URL + r'/get'
 
     @staticmethod
     def send_get():
