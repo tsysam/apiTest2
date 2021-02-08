ac810bd8875cbfd2df4bf305b81f91b80096b7b2
amorzeev <aleksandr.morzeev@t-systems.com>
2021-02-08 17:50:41 +0300

    cleanup

--- tests/test_get.py
+++ tests/test_get.py
@@ -3,7 +3,7 @@ from core.api import Api
 
 
 def test_simple_get():
-    response = requests.get('https://httpbin.org/get', verify=False)
+    response = requests.get('https://httpbin.org/get')
     assert response.status_code == 200
 
 
